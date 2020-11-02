8854290b1a9622b80063020a6159a004b2fc1c2c
Mesbah Himel <mesbah0010@gmail.com>
2020-11-02 10:39:48 +0600

    first commit

--- LAB_TASK_3.1_PHP/name/nameCheck.php
+++ LAB_TASK_3.1_PHP/name/nameCheck.php
@@ -4,11 +4,11 @@
 	//echo $_GET['name'];
 
 	echo $_POST['name'];
-	echo $_POST['email'];
+	/*echo $_POST['email'];
 	echo $_POST['dob'];
 	echo $_POST['gender'];
 	echo $_POST['degree'];
 	echo $_POST['blood group'];
-
+	*/
 
 ?>
