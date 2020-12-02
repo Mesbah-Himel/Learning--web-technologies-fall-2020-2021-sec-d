378bff8769b96465c2d5781eb2b113197361adda
Mesbah Himel <mesbah0010@gmail.com>
2020-12-02 09:23:31 +0600

    first commit

---
+++ FINAL_LAB_TASK_1.2_PHP/employee_info/view/registration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Management</title>
+</head>
+<body>
+    <fieldset>
+        <legend><h2>Emp_Registration</h2></legend><br>
+        <form action="" method="post">
+
+            Name:<input type="name" name="name" required><br><hr>
+            Contact no:<input type="tel" name="number" required><br><hr>
+            Username:<input type="text" name="username" required><br><hr>
+            Password:<input type="password" name="password" required><br><hr>
+            <input type="submit" name="submit" value="submit">
+        </form>
+
+    </fieldset>
+</body>
+</html>
